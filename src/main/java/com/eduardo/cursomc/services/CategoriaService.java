4802352e3904492b95b3def96fb7c789de4568ce
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2019-04-21 19:07:33 -0300

    Deletando categoria com mensagem personalizada de erro para categorias com produtos

--- src/main/java/com/eduardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.eduardo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.eduardo.cursomc.domain.Categoria;
 import com.eduardo.cursomc.domain.repositories.CategoriaRepository;
+import com.eduardo.cursomc.services.exception.DataIntegrityException;
 import com.eduardo.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 }
