553355830fd12e43f93a122b3689bacd6ec90e0e
Eduardo de Carvalho <ecarvalho@segware.com.br>
2019-08-18 20:16:58 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/eduardo/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/eduardo/cursomc/dto/ClienteNewDTO.java
@@ -9,131 +9,139 @@ import org.hibernate.validator.constraints.Length;
 
 public class ClienteNewDTO implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	private String nome;
-	private String email;
-	private String cpfOuCpnj;
-	private Integer tipo;
-	
-	private String logradouro;
-	private String numero;
-	private String complemento;
-	private String bairro;
-	private String cep;
-	
-	private String telefone1;
-	private String telefone2;
-	private String telefone3;
-	
-	private Integer cidadeId;
-	
-	public ClienteNewDTO() {
-	}
+    private static final long serialVersionUID = 1L;
 
-	public String getNome() {
-		return nome;
-	}
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    @Length(min = 5, message = "O tamanho deve ser entre 5 e 80 caractéries.")
+    private String nome;
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    @Email(message = "Email inválido")
+    private String email;
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    private String cpfOuCnpj;
+    private Integer tipo;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    private String cep;
 
-	public String getEmail() {
-		return email;
-	}
+    @NotEmpty(message = "Preenchimento obrigatório.")
+    private String telefone1;
+    private String telefone2;
+    private String telefone3;
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    private Integer cidadeId;
 
-	public String getCpfOuCpnj() {
-		return cpfOuCpnj;
-	}
+    public ClienteNewDTO() {
+        // construtor vazio
+    }
 
-	public void setCpfOuCpnj(String cpfOuCpnj) {
-		this.cpfOuCpnj = cpfOuCpnj;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public Integer getTipo() {
-		return tipo;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setTipo(Integer tipo) {
-		this.tipo = tipo;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public String getLogradouro() {
-		return logradouro;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public void setLogradouro(String logradouro) {
-		this.logradouro = logradouro;
-	}
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
 
-	public String getNumero() {
-		return numero;
-	}
+    public void setCpfOuCnpj(String cpfOuCpnj) {
+        this.cpfOuCnpj = cpfOuCpnj;
+    }
 
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
+    public Integer getTipo() {
+        return tipo;
+    }
 
-	public String getComplemento() {
-		return complemento;
-	}
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
 
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
+    public String getLogradouro() {
+        return logradouro;
+    }
 
-	public String getBairro() {
-		return bairro;
-	}
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
 
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
+    public String getNumero() {
+        return numero;
+    }
 
-	public String getCep() {
-		return cep;
-	}
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
 
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
+    public String getComplemento() {
+        return complemento;
+    }
 
-	public String getTelefone1() {
-		return telefone1;
-	}
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 
-	public void setTelefone1(String telefone1) {
-		this.telefone1 = telefone1;
-	}
+    public String getBairro() {
+        return bairro;
+    }
 
-	public String getTelefone2() {
-		return telefone2;
-	}
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
 
-	public void setTelefone2(String telefone2) {
-		this.telefone2 = telefone2;
-	}
+    public String getCep() {
+        return cep;
+    }
 
-	public String getTelefone3() {
-		return telefone3;
-	}
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 
-	public void setTelefone3(String telefone3) {
-		this.telefone3 = telefone3;
-	}
+    public String getTelefone1() {
+        return telefone1;
+    }
 
-	public Integer getCidadeId() {
-		return cidadeId;
-	}
+    public void setTelefone1(String telefone1) {
+        this.telefone1 = telefone1;
+    }
 
-	public void setCidadeId(Integer cidadeId) {
-		this.cidadeId = cidadeId;
-	}
-	
-	
+    public String getTelefone2() {
+        return telefone2;
+    }
+
+    public void setTelefone2(String telefone2) {
+        this.telefone2 = telefone2;
+    }
+
+    public String getTelefone3() {
+        return telefone3;
+    }
+
+    public void setTelefone3(String telefone3) {
+        this.telefone3 = telefone3;
+    }
+
+    public Integer getCidadeId() {
+        return cidadeId;
+    }
+
+    public void setCidadeId(Integer cidadeId) {
+        this.cidadeId = cidadeId;
+    }
+
 }
