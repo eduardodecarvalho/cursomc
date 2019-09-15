61afc72e6596ba6e1af36e818b833e560a8a3b2a
eduardodecarvalho <ecarvalho@segware.com.br>
2019-09-15 13:17:05 -0300

    Implementando toString de Pedido

--- src/main/java/com/eduardo/cursomc/domain/Pedido.java
+++ src/main/java/com/eduardo/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.eduardo.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -108,4 +111,25 @@ public class Pedido implements Serializable {
         this.itens = itens;
     }
 
+    @Override
+    public String toString() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        StringBuilder builder = new StringBuilder();
+        builder.append("Pedido número: ");
+        builder.append(getId());
+        builder.append(", Instante: ");
+        builder.append(sdf.format(getInstante()));
+        builder.append(", Cliente: ");
+        builder.append(getCliente().getNome());
+        builder.append(", Situação do pagamento: ");
+        builder.append(getPagamento().getEstado().getDescricao());
+        builder.append("\nDetalhes:\n");
+        for (ItemPedido ip : getItens()) {
+            builder.append(ip.toString());
+        }
+        builder.append("Valor total: ");
+        builder.append(nf.format(getValorPedido()));
+        return builder.toString();
+    }
 }
